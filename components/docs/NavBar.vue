5da3b4c9e2f49bd996c0793c6292807139af9ca2
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-05-18 14:19:45 +0300

    feat: added social links

--- components/docs/NavBar.vue
+++ components/docs/NavBar.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 interface MenuItem {
-  label: string
+  icon: string
   href: string
 }
 
 const menu: MenuItem[] = [
   {
-    label: 'Home',
-    href: '/',
+    icon: 'i-ph-github-logo-fill',
+    href: 'https://github.com/oku-ui',
   },
   {
-    label: 'Primitives',
-    href: '/docs/primitives/overview/introduction',
+    icon: 'i-ph-discord-logo-fill',
+    href: 'https://chat.productdevbook.com',
+  },
+  {
+    icon: 'i-ph-twitter-logo-fill',
+    href: 'https://twitter.com/oku_ui',
   },
 ]
 </script>
@@ -25,20 +29,13 @@ const menu: MenuItem[] = [
             <img src="/logo.svg" alt="Oku" class="h-6 w-auto">
           </a>
         </div>
-        <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 u-text-gray-700">
-            <span class="sr-only">Open main menu</span>
-            <div class="i-ph-list-bold text-white  h-5 w-5" />
-          </button>
-        </div>
-        <div class="hidden lg:flex lg:min-w-0 lg:justify-center lg:flex-1 lg:gap-x-12">
+        <div class="flex lg:min-w-0 lg:justify-end lg:flex-1 gap-x-6">
           <NuxtLink
             v-for="item in menu" :key="item.href"
             :to="item.href"
-            class="text-gray-50 text-sm focus:outline-none font-bold hover:text-cyan-600 focus:text-gray-50"
-          >
-            {{ item.label }}
-          </NuxtLink>
+            :class="item.icon"
+            class="text-gray-50 text-2xl focus:outline-none font-bold hover:text-cyan-600 focus:text-gray-50"
+          />
         </div>
       </nav>
     </div>
