9cb60f7b4e65714bd2d97d51e05554f6d618cb35
OliveLeb <lebel.olivier@wanadoo.fr>
2023-05-26 19:11:12 +0200

    fix: check if slug in params before checking for typo

--- middleware/typo.global.ts
+++ middleware/typo.global.ts
@@ -1,4 +1,8 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.params.slug.includes('aspet-ratio'))
-    return navigateTo('/docs/primitives/components/aspect-ratio', { redirectCode: 301 })
+export default defineNuxtRouteMiddleware((to) => {
+  if (to.name === 'docs-slug') {
+    if ('slug' in to.params) {
+      if (to.params.slug.includes('aspet-ratio'))
+        return navigateTo('/docs/primitives/components/aspect-ratio', { redirectCode: 301 })
+    }
+  }
 })
