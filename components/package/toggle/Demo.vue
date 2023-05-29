842be2debcb6e81791bda50cdbc1ad42c9ad22fb
Mehmet <hi@productdevbook.com>
2023-05-29 21:15:16 +0300

    feat: toggle (#24)

---
+++ components/package/toggle/Demo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { OkuToggle } from '@oku-ui/toggle'
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <OkuToggle
+      id="toggle" aria-label="Toggle italic"
+      class="w-10 h-10 border-2 rounded bg-white/20 text-black flex items-center justify-center data-[state=on]:border-green-500 data-[state=on]:bg-white data-[state=off]:border-gray-900 hover:bg-gray-400"
+    >
+      I
+    </OkuToggle>
+  </div>
+</template>
