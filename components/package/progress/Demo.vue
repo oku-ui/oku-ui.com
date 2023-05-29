b0d1629adbbec71bac93e4184da81ff6a89af5e8
Mehmet <hi@productdevbook.com>
2023-05-29 21:53:14 +0300

    feat: progress (#25)

---
+++ components/package/progress/Demo.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable no-console -->
+<script setup lang="ts">
+import { OkuProgress, OkuProgressIndicator } from '@oku-ui/progress'
+import { ref } from 'vue'
+
+// #1
+const progress = ref<number | null>(10)
+
+onMounted(() => {
+  const timer = setTimeout(() => progress.value = 66, 1000)
+  return () => clearTimeout(timer)
+})
+
+watch(progress, res =>
+  console.log(res))
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <OkuProgress class="relative overflow-hidden rounded-full w-300px h-25px bg-blue-500 transform translate-z-0" :value="progress">
+      <OkuProgressIndicator
+        class="w-full h-full transition duration-600 ease-out bg-white"
+        :style="{
+          transform: `translateX(${progress}%)`,
+        }"
+      />
+    </OkuProgress>
+  </div>
+</template>
