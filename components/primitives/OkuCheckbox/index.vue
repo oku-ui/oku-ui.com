aadec647eb76ad966daa62206500dba01ab91a3d
productdevbook <hi@productdevbook.com>
2023-07-15 17:24:07 +0300

    fix: icon

--- components/primitives/OkuCheckbox/index.vue
+++ components/primitives/OkuCheckbox/index.vue
@@ -9,7 +9,7 @@ const refV = ref<CheckboxRef>()
   <div class="flex items-center justify-center">
     <OkuCheckbox id="c1" ref="refV" class="w-6 h-6 flex bg-white rounded-md text-red-500 checked:text-red-600">
       <OkuCheckboxIndicator class="w-6 h-6 flex items-center justify-center text-blue-500">
-        <div class="i-ph-check-bold w-5 h-5" />
+        <div class="i-heroicons-check w-5 h-5" />
       </OkuCheckboxIndicator>
     </OkuCheckbox>
     <label class="Label" for="c1">
