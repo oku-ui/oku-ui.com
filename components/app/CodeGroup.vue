af84b52465b2f37df3fde746ad1006544df6697b
emirulucay <eux202@gmail.com>
2023-07-19 18:02:36 +0300

    feat: component background changed

--- components/app/CodeGroup.vue
+++ components/app/CodeGroup.vue
@@ -23,18 +23,18 @@ function changeTab(index: number) {
 
 <template>
   <div class="overflow-hidden">
-    <div class="bg-gradient-to-br rounded-lg from-oku-100 to-oku-600 w-full relative items-center justify-center flex">
+    <div class="bg-gradient-to-br rounded-lg from-[#FF0F7B] to-[#F89B29] w-full relative items-center justify-center flex">
       <div class="w-full max-w-xl flex flex-col items-center justify-center py-24">
         <slot name="preview" />
       </div>
     </div>
     <slot name="code">
       <div :selected-index="selectedIndex" class="mt-5" @change="changeTab(selectedIndex)">
-        <div class="flex border border-oku-200 dark:border-oku-700 bg-oku-50 dark:bg-oku-950 rounded mb-4 overflow-hidden">
+        <div class="flex border border-[#D3D3D3] dark:border-[#22201E] bg-[#F2F2F2] dark:bg-[#121110] rounded mb-4 overflow-hidden">
           <div v-for="(tab, index) in tabs" :key="index" as="template" @click="selectedIndex = index">
             <button
               class="px-4 py-2 focus:outline-none border-r-oku-200 dark:border-r-oku-700"
-              :class="[selectedIndex === index ? 'font-medium text-oku-950 dark:text-white bg-oku-200 dark:bg-oku-800' : 'hover:bg-oku-200 dark:hover:bg-oku-800']"
+              :class="[selectedIndex === index ? 'font-medium text-[#111] dark:text-white bg-[#C9C9C9] dark:bg-[#22201E]' : 'hover:bg-[#C9C9C9] dark:hover:bg-[#22201E]']"
             >
               {{ tab.label }}
             </button>
