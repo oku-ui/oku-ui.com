d695f1ba8bd2d5f0a9e55cc83481ef1c31eedc28
emirulucay <eux202@gmail.com>
2023-09-29 14:41:19 +0300

    fix: radix component looks originally anymore

--- components/primitives/OkuAccordion/index.vue
+++ components/primitives/OkuAccordion/index.vue
@@ -34,9 +34,9 @@ defineOptions({
   <template v-for="(item,index) in data" :key="index">
     <OkuAccordionItem class="focus-within:shadow-grayOA-950 dark:focus-within:shadow-grayOA-200 mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]" :value="item.value">
       <OkuAccordionHeader class="flex">
-        <OkuAccordionTrigger class="text-grayOA-950 dark:text-white shadow-grayOA-300 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white dark:bg-grayOA-950 px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none">
+        <OkuAccordionTrigger class="text-grayOA-950 group dark:text-white shadow-grayOA-300 hover:bg-mauve2 group flex h-[45px] flex-1 cursor-default items-center justify-between bg-white dark:bg-grayOA-950 px-5 text-[15px] leading-none shadow-[0_1px_0] outline-none">
           {{item.title}}
-         <i class="i-ic-baseline-keyboard-arrow-down h-3.5 w-3.5 !text-black dark:!text-white" />
+         <i class="i-ic-baseline-keyboard-arrow-down h-3.5 w-3.5 !text-black dark:!text-white ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180" />
 
         </OkuAccordionTrigger>
       </OkuAccordionHeader>
