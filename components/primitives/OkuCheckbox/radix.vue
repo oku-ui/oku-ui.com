2c0788deff6fe4e16d5b9d1549fe886ca6163784
Emir Uluçay <74816727+emirulucay@users.noreply.github.com>
2023-10-01 16:08:18 +0300

    fix: radix checkbox bugs fixed (#82)

--- components/primitives/OkuCheckbox/radix.vue
+++ components/primitives/OkuCheckbox/radix.vue
@@ -10,7 +10,7 @@ import { OkuCheckbox, OkuCheckboxIndicator } from '@oku-ui/checkbox'
         class="shadow-blackA7 hover:bg-violet3 flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-[4px] bg-white shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px_black]"
       >
         <OkuCheckboxIndicator class="text-violet11">
-          <div class="i-heroicons-check w-5 h-5" />
+          <div class="i-heroicons-check flex w-[15px] h-[15px]" />
         </OkuCheckboxIndicator>
       </OkuCheckbox>
       <label class="pl-[15px] text-[15px] leading-none text-white" for="c1">
