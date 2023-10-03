4ca28c68af1e2ec2b6e32e53a6ce476523408848
productdevbook <hi@productdevbook.com>
2023-10-03 18:40:52 +0300

    chore: upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,43 @@
-// import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config'
 
-// export default antfu(
-//   {
-//     rules: {
-//       "no-console": "warn",
-//       "@typescript-eslint/ban-ts-comment": "off",
-//       "n/prefer-global/process": "off"
-//     }
-//   }
-// )
-
-var antfu = require('@antfu/eslint-config');
-
-module.exports = antfu({
-  rules: {
-    "no-console": "warn",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "n/prefer-global/process": "off"
-  }
-});
+export default antfu(
+  {},
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'migrations/',
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.nuxt',
+      '**/.output',
+      '**/.github',
+      '**/coverage',
+      '**/nuxt.d.ts',
+      '**/.DS_Store',
+      '**/.vscode',
+      '**/**.yml',
+    ],
+  },
+  {
+    rules: {
+      'no-console': 'warn',
+      'vue/no-deprecated-slot-attribute': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'import/order': 'off',
+      'n/prefer-global/process': 'off',
+      'node/prefer-global/process': 'off',
+      'ts/ban-ts-comment': 'off',
+      'ts/consistent-type-definitions': 'off',
+    },
+  },
+  {
+    files: [
+      '**/tests/**/*.ts',
+    ],
+    rules: {
+      'unused-imports/no-unused-vars': 'off',
+      'no-console': 'off',
+    },
+  },
+)
